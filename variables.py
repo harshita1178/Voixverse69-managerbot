6b3fe1d89db98e527aa0860178b18aeb5449a98f
harshita1178 <usekarlomujhe@gmail.com>
2025-05-26 14:27:13 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -19,31 +19,31 @@ class Config(object):
 
     # <================================================ REQUIRED ======================================================>
     # Telegram API configuration
-    API_ID = 204 # Get this value from my.telegram.org/apps
-    API_HASH = ""
+    API_ID = 20952516 # Get this value from my.telegram.org/apps
+    API_HASH = "4b7997216605cd4dd9cb2ee513838955"
 
     # Database configuration (PostgreSQL)
-    DATABASE_URL = "postgres:"
+    DATABASE_URL = "postgresql://dbvoixverse_user:FS7FWzyHyts2wUgYqrn6b702Wqo4KahS@dpg-d0q286odl3ps73b8hbc0-a.oregon-postgres.render.com/dbvoixverse"
 
     # Event logs chat ID and message dump chat ID
-    EVENT_LOGS = -100
-    MESSAGE_DUMP = -100
+    EVENT_LOGS = -1002617754650
+    MESSAGE_DUMP = -1002331557980
 
     # MongoDB configuration
-    MONGO_DB_URI = ""
+    MONGO_DB_URI = "mongodb+srv://VOIXVERSE1789:ANIMELOVER11@voixverse-managerbot.m11s3b8.mongodb.net/?retryWrites=true&w=majority&appName=Voixverse-managerbot"
 
     # Support chat and support ID
-    SUPPORT_CHAT = ""
-    SUPPORT_ID = -100
+    SUPPORT_CHAT = "https://t.me/VoixVerse_Studioss"
+    SUPPORT_ID = -1002668191611
 
     # Database name
-    DB_NAME = ""
+    DB_NAME = "Voixverse-managerbot"
 
     # Bot token
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "7581829695:AAGCcqcbUBuyb8cpBpwpE9OBzxHaILWeNIA"  # Get bot token from @BotFather on Telegram
 
     # Owner's Telegram user ID (Must be an integer)
-    OWNER_ID = 5907205317
+    OWNER_ID = 6675050163
     # <=======================================================================================================>
 
     # <================================================ OPTIONAL ======================================================>
